c3f78eb11a5a43c2bbb8339a6e5708b5e71aa86f
Aditya Tiwari <adityatiwari.py@gmail.com>
2026-07-14 10:51:12 -0700

    Add Node class with data and next reference

---
+++ as.java
@@ -0,0 +1,10 @@
+ static class Node {
+        int data;       // Data stored in the node
+        Node next;      // Reference to the next node
+
+        // Constructor to initialize node data
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
